a0103a0b41952cab1cdefd527066cdc9205b89e7
colo1211 <colo1211@naver.com>
2021-08-02 11:11:15 +0900

    이벤트리스너 반복문

--- part2/boot/tab.js
+++ part2/boot/tab.js
@@ -1,32 +1,17 @@
 // function init 내에서 jQuery 사용
 function init() {
-    // tab1 버튼을 누르면
-     $('.tab-button').eq(0).on('click', function () {
+    
+    for (let i=0; i<$('.tab-button').length; i++){
+     $('.tab-button').eq(i).on('click', function () {
+         console.log(i);
          // 나머지 모든 tab에 대해서 class를 제거한다.(eq를 안달면 모든 해당 class가 선택되어 remove할 수 있다.) 
          $('.tab-button').removeClass('active');
          $('.tab-content').removeClass('show');
          // 제거 이후에 tab에서 0번째 탭에 class를 추가한다. 
-         $('.tab-button').eq(0).addClass('active');
-         $('.tab-content').eq(0).addClass('show');
-    });
-    //tab2 버튼을 누르면
-         $('.tab-button').eq(1).on('click', function () {
-         $('.tab-button').eq(0).removeClass('active');
-         $('.tab-content').eq(0).removeClass('show');
-         $('.tab-button').eq(1).addClass('active');
-         $('.tab-content').eq(1).addClass('show');
-         $('.tab-button').eq(2).removeClass('active');
-         $('.tab-content').eq(2).removeClass('show');
-    });
-    // tab3 버튼을 누르면
-         $('.tab-button').eq(2).on('click', function () {
-         $('.tab-button').eq(0).removeClass('active');
-         $('.tab-content').eq(0).removeClass('show');
-         $('.tab-button').eq(1).removeClass('active');
-         $('.tab-content').eq(1).removeClass('show');
-         $('.tab-button').eq(2).addClass('active');
-         $('.tab-content').eq(2).addClass('show');
-    });
+         $('.tab-button').eq(i).addClass('active');
+         $('.tab-content').eq(i).addClass('show');
+        });
+    }
 }
 
 var scriptElement = document.createElement('script');
